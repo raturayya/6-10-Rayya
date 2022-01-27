0f40e895294c90da0704000b175f340b1054fbd5
raturayya <raturayya08@gmail.com>
2022-01-27 19:36:19 +0700

    laravel

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "nama" => "Balqies Lintang Ratu Rayya",
         "email" => "raturayya08@gmail.com",
         "gambar" => "rayya.jpg"
@@ -26,5 +29,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/gallery', function () {
-    return view('gallery');
+    return view('gallery', [
+        "title" => "Gallery"
+    ]);
 });
