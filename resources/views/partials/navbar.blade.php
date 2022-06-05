60e4c9b3471989fd90480a5377b91f687d82c677
raturayya <raturayya08@gmail.com>
2022-06-05 10:11:44 +0700

    modul 6-7

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,31 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
-      <div class="container">
-        <a class="navbar-brand" href="/">Rayya</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
+  <div class="container">
+    <a class="navbar-brand" href="/">AxxA</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
           <a class="nav-link {{ ( $title === 'Home') ? 'active' : '' }}" aria-current="page" href="/">Home</a>
-          </li>
-          <li class="nav-item">
+        </li>
+        <li class="nav-item">
           <a class="nav-link {{ ( $title === 'About') ? 'active' : '' }}" href="/about">About</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}" href="/">Gallery</a>
-          </li>
-          <li class="nav-item">
-          <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="/">Contact Us</a>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="{{route ('login')}}">Login</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="{{route ('register')}}">Register</a>
-          </li>
-      </div>
-      </div>
-    </nav>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ( $title === 'Gallery') ? 'active' : '' }}" href="/gallery">Gallery</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ( $title === 'Contacts') ? 'active' : '' }}" href="{{ route('contacts.create')}}">Contact Us</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="{{route ('login')}}">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route ('register')}}">Register</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
